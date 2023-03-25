cd944e5d608ab7989b784dac891c55e9705cfcaf
alan <alan.ghb@gmail.com>
2023-03-25 18:07:52 -0300

    atualiza

--- galeria/views.py
+++ galeria/views.py
@@ -2,7 +2,20 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'galeria/index.html')
+
+    dados = {
+        1: {"nome": "Nebulosa de Carina",
+            "legenda": "weebtelescope.org / NASA / James Webb"
+            },
+        2: {"nome": "Galáxia NGC 1979",
+            "legenda": "nasa.org / NASA / Hubble"
+            },
+        3: {"nome": "Galáxia NGC 1979 OUTRO",
+            "legenda": "nasa.org / NASA / Hubble 2"
+            },
+    }
+
+    return render(request, 'galeria/index.html', {"cards": dados})
 
 
 def imagem(request):
